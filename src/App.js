5fd32022e15f7691175148f1b099b72a0128892e
Basant14321 <66409794+basant14321@users.noreply.github.com>
2021-06-02 23:23:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
+
+function App() {
+  const [search, setSerach] = useState("Banana");
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = "74b54984";
+  const APP_KEY = "9d07a66f9c77b457fd1c98e463651248";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${"74b54984"}&app_key=${"9d07a66f9c77b457fd1c98e463651248"}`
+    );
+    setRecipes(res.data.hits);
+  };
+
+  const onInputChange = e => {
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
+  return (
+    <div className="App">
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
